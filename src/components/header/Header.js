8eff3118ff2025a5404423cf7305b01260f77ca2
Agueda Perez <aguedapm@yahoo.com>
2019-05-27 10:58:36 +0200

    Added white background on click on tabs

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import {NavLink } from 'react-router-dom';
 
 import Tab from './Tab'
@@ -6,15 +6,39 @@ import yoshiplogo from "../../assets/images/yoship-logo.png";
 import "./Header.css"
 
 
-const Header = () => {
+class Header extends Component{
 
-  return  <header className="header_header">
-    <NavLink exact to="/" activeClassName="selected" className="header_link"><img src={yoshiplogo} alt="yoship logo" className="header_logo"/>YoShip</NavLink>
-    <NavLink to="/tracking" activeClassName="selected" className="header_link"><Tab icon="map-marker-alt" tab="Tracking"/></NavLink>
-    <NavLink to="/login" activeClassName="selected" className="header_link"><Tab icon="user-tie" tab="Login"/></NavLink>
-    <NavLink to="/register" activeClassName="selected" className="header_link"><Tab icon="user-plus" tab="Register"/></NavLink>
-  </header>
+  state = {
+    background : true
+  }
 
+  whiteBackground = () => {
+    this.setState({background : false}) 
+  }
+
+  pictureBackground = () => {
+    this.setState({background : true})
+  }
+  
+  render () {
+
+    return  (
+    <header className={this.state.background? "header_header" : "header_header_white"}>
+      <NavLink exact to="/" activeClassName="selected" className={this.state.background? "header_link" : "header_link_black"} onClick={this.pictureBackground}>
+        <img src={yoshiplogo} alt="yoship logo" className="header_logo"/>YoShip
+      </NavLink>
+      <NavLink to="/tracking" activeClassName="selected" className={this.state.background? "header_link" : "header_link_black"} onClick={this.whiteBackground}>
+        <Tab icon="map-marker-alt" tab="Tracking"/>
+      </NavLink>
+      <NavLink to="/login" activeClassName="selected" className={this.state.background? "header_link" : "header_link_black"} onClick={this.whiteBackground}>
+        <Tab icon="user-tie" tab="Login"/>
+      </NavLink>
+      <NavLink to="/register" activeClassName="selected" className={this.state.background? "header_link" : "header_link_black"} onClick={this.whiteBackground}>
+        <Tab icon="user-plus" tab="Register"/>
+      </NavLink>
+    </header>
+    )
+  }
 }
 
 

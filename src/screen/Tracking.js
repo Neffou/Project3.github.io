8e1625af1e8041917e3cd5f59f6848f380acf110
Monia-L <monia.laoufi@gmail.com>
2019-05-28 16:42:21 +0200

    last version work's monia

---
+++ src/screen/Tracking.js
@@ -0,0 +1,17 @@
+import React from "react";
+import TrackingSection from "../components/TrackingSection/TrackingSection/TrackingSection";
+
+
+const Tracking = () => {
+
+    return  (        
+    <div>
+
+<TrackingSection />
+
+    </div>
+
+)}
+
+
+export default Tracking;
